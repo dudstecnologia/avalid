eabfbcc64ac984f5d71f3a95d8ecb83b67b86530
Eduardo <eduardo@dudstecnologia.com.br>
2020-03-18 23:25:31 -0400

    Tela de Login - Finalizada

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,20 @@ import route from 'ziggy'
 import VueMeta from 'vue-meta'
 import { InertiaApp } from '@inertiajs/inertia-vue'
 import { Ziggy } from '../assets/js/ziggy'
+import Vuesax from 'vuesax'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'vuesax/dist/vuesax.css'
+import 'material-icons/iconfont/material-icons.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.mixin({ methods: { route: (name, params, absolute) => route(name, params, absolute, Ziggy) } });
 
 Vue.use(VueMeta)
 Vue.use(InertiaApp)
+Vue.use(Vuesax)
+Vue.use(BootstrapVue)
 
 const app = document.getElementById('app')
 
