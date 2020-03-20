360e0c3fa5576fb023e33fa43a86ff3f124c141b
Eduardo <eduardo@dudstecnologia.com.br>
2020-03-20 00:51:18 -0400

    Menu como componente

--- resources/js/Componentes/Layout.vue
+++ resources/js/Componentes/Layout.vue
@@ -1,42 +1,19 @@
 <template>
     <div>
+        <menu-navbar />
 
-        <b-navbar toggleable="lg" type="dark" variant="dark">
-            <div class="container">
-                <b-navbar-brand href="#">AvaliD</b-navbar-brand>
-
-                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-                <b-collapse id="nav-collapse" is-nav>
-                    <b-navbar-nav class="ml-auto">
-                        <b-nav-item-dropdown text="Lang" right>
-                            <b-dropdown-item href="#">EN</b-dropdown-item>
-                            <b-dropdown-item href="#">ES</b-dropdown-item>
-                            <b-dropdown-item href="#">RU</b-dropdown-item>
-                            <b-dropdown-item href="#">FA</b-dropdown-item>
-                        </b-nav-item-dropdown>
-
-                        <b-nav-item-dropdown right>
-                            <template v-slot:button-content>
-                            <em>User</em>
-                            </template>
-                            <b-dropdown-item href="#">Profile</b-dropdown-item>
-                            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                        </b-nav-item-dropdown>
-
-                        <b-nav-item href="/logout" right>Sair</b-nav-item>
-                    </b-navbar-nav>
-                </b-collapse>
-            </div>
-        </b-navbar>
-
-
-        <slot />
+        <div class="container">
+            <slot />
+        </div>
     </div>
 </template>
 
 <script>
+import MenuNavbar from '../Componentes/MenuNavbar'
 export default {
+    components: {
+        MenuNavbar
+    },
     data() {
         return {
 
---
+++ resources/js/Componentes/MenuNavbar.vue
@@ -0,0 +1,47 @@
+<template>
+<span>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+        <div class="container">
+            <b-navbar-brand href="#">AvaliD</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item-dropdown text="Lang" right>
+                        <b-dropdown-item href="#">EN</b-dropdown-item>
+                        <b-dropdown-item href="#">ES</b-dropdown-item>
+                        <b-dropdown-item href="#">RU</b-dropdown-item>
+                        <b-dropdown-item href="#">FA</b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+                    <b-nav-item-dropdown right>
+                        <template v-slot:button-content>
+                        <em>User</em>
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+                    <b-nav-item @click="logout()" right>Sair</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </div>
+    </b-navbar>
+</span>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        logout() {
+            this.$inertia.post(this.route('logout'))
+        }
+    }
+}
+</script>
